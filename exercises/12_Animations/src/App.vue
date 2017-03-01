116a0875730936b7fcf0d3015dd1f901d5a6c71a
Kirill Rebricov <stique@reg.ru>
2017-03-01 18:28:05 +0400

    animations 2

--- exercises/12_Animations/src/App.vue
+++ exercises/12_Animations/src/App.vue
@@ -3,11 +3,16 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
+                <br>
+                <select v-model="animationType">
+                    <option value="fade">fade</option>
+                    <option value="slide">slide</option>
+                </select>
                 <hr>
                 <button class="btn btn-primary" @click="show = !show">Show alert</button>
                 <br>
                 <br>
-                <transition name="fade">
+                <transition :name="animationType">
                     <div class="alert alert-info" v-if="show">Some info!</div>
                 </transition>
                 <transition name="slide" type="animation">
@@ -18,6 +23,28 @@
                     leave-active-class="animated shake">
                     <div class="alert alert-info" v-if="show">Some info!</div>
                 </transition>
+                <transition :name="animationType" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">Some info!</div>
+                    <div class="alert alert-warning" v-else key="warning">Some warning!</div>
+                </transition>
+                <hr>
+                <button class="btn btn-primary" @click="load = !load">Load | Remove element</button>
+                <br>
+                <br>
+                <transition
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled"
+
+                    :css="false">
+                    <div style="width: 300px; height: 100px; background-color: lightgreen;" v-if="load"></div>
+                </transition>
             </div>  
         </div>
     </div>
@@ -27,8 +54,59 @@
     export default {
         data() {
             return {
-                show: false
+                show: false,
+                load: true,
+                animationType: 'fade',
+                elementWidth: 100,
             }
+        },
+        methods: {
+            beforeEnter(el) {
+                console.log('beforeEnter');
+                this.elementWidth = 100;
+                el.style.width = this.elementWidth;
+            },
+            enter(el, done) {
+                console.log('enter');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = ( this.elementWidth + round * 10 ) + 'px';
+                    round++;
+                    if ( round > 20 ) {
+                        clearInterval( interval );
+                        done();
+                    }
+                }, 20);
+            },
+            afterEnter(el) {
+                console.log('afterEnter');
+            },
+            enterCancelled(el) {
+                console.log('enterCancelled');
+            },
+            beforeLeave(el) {
+                console.log('beforeLeave');
+                this.elementWidth = 300;
+                el.style.width = this.elementWidth + 'px';
+            },
+            leave(el, done) {
+                console.log('leave');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = ( this.elementWidth - round * 10 ) + 'px';
+                    round++;
+                    if ( round > 20 ) {
+                        clearInterval( interval );
+                        done();
+                    }
+                }, 20);
+            },
+            afterLeave(el) {
+                console.log('afterLeave');
+            },
+            leaveCancelled(el) {
+                console.log('leaveCancelled');
+            },
         }
     }
 </script>
