e0f164c17f8f923f7df26e251fa2b8ba7cb95114
Kirill Rebricov <stique@reg.ru>
2017-03-03 10:58:11 +0400

    quiz and anomated monster slayer

---
+++ projects/3_Quiz/src/components/Answer.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="alert alert-success text-center">
+		<h1>That's correct!</h1>
+		<hr>
+		<button class="btn btn-primary" @click="onNextQuestion">Next question</button>
+	</div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+	export default {
+		methods: {
+			onNextQuestion() {
+				this.$emit('confirmed');
+			}
+		}
+	}
+</script>
