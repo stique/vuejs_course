411d14a0e21546a651301d8177d6c5be9260e533
Kirill Rebricov <stique@reg.ru>
2017-03-05 09:47:38 +0400

    routing

---
+++ exercises/14_routing/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+                <hr>
+                <router-view name="header-top"></router-view>
+                <transition name="fade" mode="out-in">
+                	<router-view></router-view>
+                </transition>
+                <router-view name="header-bottom"></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	import Header from './components/Header.vue';
+    export default {
+    	components: {
+    		appHeader: Header
+    	}
+    }
+</script>
+
+<style>
+.fade-enter {
+	opacity: 0;
+}
+.fade-enter-active {
+    transition: opacity 1s;
+}
+.fade-leave {
+    opacity: 1;
+}
+.fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+    position: absolute;
+}
+.fade-move {
+    transition: transform 1s;
+}
+</style>
