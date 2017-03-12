a09a768e92a873c1c184bdd3d4cc2927def4f789
Kirill Rebricov <stique@reg.ru>
2017-03-12 11:38:58 +0400

    traider start

---
+++ exercises/15_vuex/src/components/AnotherCounter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="asyncIncrement({by: 50, duration: 5000})">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by: 50, duration: 5000})">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import { mapActions } from 'vuex';
+    export default {
+        methods: {
+            ...mapActions([
+                'asyncIncrement',
+                'asyncDecrement'
+            ])
+        }
+    }
+</script>
---
+++ exercises/15_vuex/src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment(50)">Increment</button>
+        <button class="btn btn-primary" @click="decrement(100)">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import { mapMutations } from 'vuex';
+    export default {
+        methods: {
+            ...mapMutations([
+                'increment',
+                'decrement'
+            ])
+        }
+    }
+</script>
